e10341acff6f6ed014cf6de0c6cabcfc8754cfd3
Fernando Parra <thefer767@gmail.com>
2025-02-24 18:56:37 -0400

    idk

--- src/app/api/auth/[...nextauth]/route.js
+++ src/app/api/auth/[...nextauth]/route.js
@@ -34,7 +34,7 @@ const handler = NextAuth({
           return session;
         },
       },
-      secret:process.env.NO_SECRET,
+      secret:process.env.NEXTAUTH_SECRET,
       pages: {
         signIn: "/signin",
       },
