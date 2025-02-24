51f12d8ed6f31bb50b9f914070292b55ad1d9bcb
Fernando Parra <thefer767@gmail.com>
2025-02-24 18:21:22 -0400

    better middleware

--- src/middleware.js
+++ src/middleware.js
@@ -4,24 +4,28 @@ import { NextResponse } from "next/server";
 export async function middleware(req) {
   const token = await getToken({ req, secret: process.env.NEXTAUTH_SECRET });
 
+  // If no token is found, redirect to the login page
   if (!token) {
     return NextResponse.redirect(new URL("/api/auth/signin", req.url));
   }
 
   const adminRoutes = ["/NewStudent", "/NewGrade", "/ManageGrades", "/ManageStudents", "/EditStudent", "/EditGrade"];
-  const userRoutes = ["/HomeClass", "/class", "/UserClasses"]; // Add your user-specific routes here
+  const userRoutes = ["/HomeClass", "/class"]; // Add your user-specific routes here
 
   const url = req.nextUrl.pathname;
 
+  // Check if the user is trying to access an admin route
   if (adminRoutes.includes(url) && token.user.role !== "admin") {
     return NextResponse.redirect(new URL("/", req.url));
   }
 
+  // Check if the user is trying to access a user route
   if (userRoutes.includes(url) && token.user.role !== "user" && token.user.role !== "admin") {
     return NextResponse.redirect(new URL("/", req.url));
   }
 
+  // If the token is valid and the user has the correct role, allow the request to proceed
   return NextResponse.next();
 }
 
-export const config = { matcher: ["/NewStudent", "/NewGrade", "/ManageGrades", "/ManageStudents", "/EditStudent", "/EditGrade", "/HomeClass", "/class", "/UserClasses"] };
+export const config = { matcher: ["/NewStudent", "/NewGrade", "/ManageGrades", "/ManageStudents", "/EditStudent", "/EditGrade", "/HomeClass", "/class"] };
